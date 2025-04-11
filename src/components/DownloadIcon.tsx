f7e203d90bafffb9848eb0631bc49c4bc889b2f3
Jasper Fernandez <jasperfernandez148@gmail.com>
2025-04-12 00:00:40 +0800

    add bounce twice

--- src/components/DownloadIcon.tsx
+++ src/components/DownloadIcon.tsx
@@ -15,14 +15,14 @@ const DownloadIcon = () => {
       <path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4' />
       <polyline
         points='7 10 12 15 17 10'
-        className='group-hover:animate-[bounce-down_0.5s_ease-in-out] group-hover:delay-300'
+        className='group-hover:animate-[bounce-down_1s_ease-in-out] group-hover:delay-300'
       />
       <line
         x1='12'
         x2='12'
         y1='15'
         y2='3'
-        className='group-hover:animate-[bounce-down_0.5s_ease-in-out] group-hover:delay-300'
+        className='group-hover:animate-[bounce-down_1s_ease-in-out] group-hover:delay-300'
       />
     </svg>
   );
